7533a294c3ec87c3eb22fda3048231f813693ff8
Alexandru Dreptu <alexdreptu@gmail.com>
2016-10-12 14:26:39 +0300

    add info printing via tests

---
+++ uname_test.go
@@ -0,0 +1,51 @@
+package sysinfo
+
+import "testing"
+
+func TestGetDomainName(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Domainname: %s\n", uname.GetDomainName())
+}
+
+func TestGetMachine(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Machine: %s\n", uname.GetMachine())
+}
+
+func TestGetNodeName(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Nodename: %s\n", uname.GetNodeName())
+}
+
+func TestGetRelease(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Release: %s\n", uname.GetRelease())
+}
+
+func TestGetSysName(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Sysname: %s\n", uname.GetSysName())
+}
+
+func TestGetVersion(t *testing.T) {
+	uname := &Uname{}
+	if err := uname.Get(); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Version: %s\n", uname.GetVersion())
+}
